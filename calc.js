55e23326c14d60f8bc4046623a2e0252df8783bd
alinlob <alinalobova17@gmail.com>
2023-08-12 21:03:27 +0300

    add calc.js

--- calc.js
+++ calc.js
@@ -1,124 +1,75 @@
-let a = ''; //first number
-let b = ''; //second number
-let sign = ''; //знак операции
-let finish = false;
+let currentValue = '';
+let currentOperation = '';
+let previousValue = '';
+let resultDisplayed = false;
 
 const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
 const action = ['-', '+', '×', '÷'];
 
-//экран
 const out = document.querySelector('.calc-screen p')
 
 const clearAll = () => {
-    a = '';
-    b = '';
-    sign = '';
-    finish = false;
-    out.textContent = 0;
+    currentValue = previousValue = currentOperation = '';
+    resultDisplayed = false;
+    out.textContent = '0';
 }
 
-document.querySelector('.ac').onclick = clearAll;
+const handleDigit = key => {
+    if (resultDisplayed) currentValue = '';
+    currentValue += key;
+    out.textContent = currentValue;
+}
 
-document.querySelector('.buttons').onclick = (event) => {
-    // Нажата не кнопка
-    if (!event.target.classList.contains('btn')) return;
-    // Нажата кнопка clearAll ac
-    if (event.target.classList.contains('ac')) {
-        clearAll();
-        return;
-    }
-
-    // Получаем нажатую кнопку
-    const key = event.target.textContent;
-
-    // Если нажата клавиша 0-9 или .
-    if (digit.includes(key)) {
-        handleDigit(key);
-    }
-
-    // Если нажата клавиша + - / *
-    if (action.includes(key)) {
-        handleAction(key);
-    }
-
-    // Если нажата клавиша %
-    if (key === '%') {
-        handlePercent();
-    }
-
-    // Если нажата клавиша +/-
-    if (key === '+/-') {
-        handlePlusMinus();
-    }
-
-    // Если нажата клавиша =
-    if (key === '=') {
-        handleEquals();
+const handleAction = key => {
+    if (currentValue) {
+        if (currentOperation) handleEquals();
+        previousValue = currentValue;
+        currentValue = '';
+        currentOperation = key;
     }
 }
 
-function handleDigit(key) {
-    if (b === '' && sign === '') {
-        a += key;
-        out.textContent = a;
-    } else if (a !== '' && b !== '' && finish) {
-        b = key;
-        finish = false;
-        out.textContent = b;
-    } else {
-        b += key;
-        out.textContent = b;
-    }
-    console.log(a, b, sign);
-}
-
-function handleAction(key) {
-    sign = key;
-    out.textContent = sign;
-    console.log(a, b, sign);
-}
-
-function handlePercent() {
-    if (b === '') {
-        a = (+a) / 100;
-        out.textContent = a;
-    } else {
-        b = (+b) / 100;
-        out.textContent = b;
+const handlePercent = () => {
+    if (currentValue) {
+        currentValue = (+currentValue / 100).toString();
+        out.textContent = currentValue;
     }
 }
 
-function handlePlusMinus() {
-    if (b === '') {
-        a = (-a).toString();
-        out.textContent = a;
-    } else {
-        b = (-b).toString();
-        out.textContent = b;
+const handlePlusMinus = () => {
+    if (currentValue) {
+        currentValue = (-currentValue).toString();
+        out.textContent = currentValue;
     }
 }
 
-function handleEquals() {
-    if (b === '') b = a;
-    switch (sign) {
-        case '+':
-            a = (+a) + (+b);
-            break;
-        case '-':
-            a = a - b;
-            break;
-        case '×':
-            a = a * b;
-            break;
-        case '÷':
-            if (b === '0') {
-                out.textContent = '∞';
-                clearAll();
-                return;
-            }
-            a = a / b;
-            break;
+const handleEquals = () => {
+    if (previousValue && currentValue && currentOperation) {
+        const a = parseFloat(previousValue);
+        const b = parseFloat(currentValue);
+        switch (currentOperation) {
+            case '+': currentValue = (a + b).toString(); break;
+            case '-': currentValue = (a - b).toString(); break;
+            case '×': currentValue = (a * b).toString(); break;
+            case '÷': currentValue = (b !== 0 ? (a / b).toString() : '∞'); break;
+        }
+        resultDisplayed = true;
+        out.textContent = currentValue;
     }
-    finish = true;
-    out.textContent = a;
+    currentOperation = '';
 }
+
+document.querySelector('.ac').addEventListener('click', clearAll);
+
+document.querySelector('.buttons').addEventListener('click', event => {
+    const target = event.target;
+    if (!target.classList.contains('btn')) return;
+
+    const key = target.textContent;
+
+    if (digit.includes(key)) handleDigit(key);
+    if (action.includes(key)) handleAction(key);
+    if (key === '%') handlePercent();
+    if (key === '+/-') handlePlusMinus();
+    if (key === '=') handleEquals();
+});
